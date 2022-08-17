4cde1b3de0d747d7ebf8168b1de693520dfe6b43
Carmen <carmencantudo@gmail.com>
2022-08-17 16:45:29 +0000

    Add fetch API and error function

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const App = () => {
+
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  const [showtData, setShowData] = useState([]);
+
+  // Define an error function
+  const loadData = () =>{
+    // get data from API
+    fetch(url, {
+      method: "GET"
+    }).then(res => res.json())  // Convert data to json
+    .then(result =>{
+      if(result){
+        setShowData(result);
+      }
+    }).catch(err =>{
+      console.log(err)
+    })
+  }
+
   return (
     <div className='container'>
 
