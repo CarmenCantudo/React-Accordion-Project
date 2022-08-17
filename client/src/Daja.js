ac9072e16a212c22cf4764f8ec7753b7c8658ef5
Carmen <carmencantudo@gmail.com>
2022-08-17 17:39:23 +0000

    Create Data.js file and create accordion function to call title and body from API

---
+++ client/src/Daja.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Data = (title, body) => {
+  return (
+    <div className='accordion-item'>
+        <h5>{title}</h5>
+        <p>{body}</p>
+    </div>
+  )
+}
+
+export default 
